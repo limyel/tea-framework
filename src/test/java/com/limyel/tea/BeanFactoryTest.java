16c97e4d7024906a828bb4f0762512918937ac04
limyel <limyel@outlook.com>
2022-05-17 08:57:04 +0800

    junit

---
+++ src/test/java/com/limyel/tea/BeanFactoryTest.java
@@ -0,0 +1,14 @@
+package com.limyel.tea;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BeanFactoryTest {
+
+    @Test
+    @DisplayName("测试 BeanFactory")
+    public void testBeanFactory() {
+
+    }
+
+}
---
+++ src/test/java/com/limyel/tea/ObjectDemo.java
@@ -0,0 +1,9 @@
+package com.limyel.tea;
+
+public class ObjectDemo {
+
+    public void greet() {
+        System.out.println("hello");
+    }
+
+}
